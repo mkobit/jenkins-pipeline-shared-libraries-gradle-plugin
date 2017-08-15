e71d40d90c86e89b643b7c4c9582a7a75bb6f860
Mike Kobit <mkobit@gmail.com>
2017-08-15 09:36:24 -0500

    Add tests for `SharedLibraryExtension`

--- src/test/kotlin/com/mkobit/jenkins/pipelines/SharedLibraryExtensionTest.kt
+++ src/test/kotlin/com/mkobit/jenkins/pipelines/SharedLibraryExtensionTest.kt
@@ -1,11 +1,10 @@
 package com.mkobit.jenkins.pipelines
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.SoftAssertions
-import org.gradle.api.Project
 import org.gradle.testfixtures.ProjectBuilder
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
-import testsupport.NotImplementedYet
 
 internal class SharedLibraryExtensionTest {
   private lateinit var sharedLibraryExtension: SharedLibraryExtension
@@ -30,33 +29,48 @@ internal class SharedLibraryExtensionTest {
 
   @Test
   internal fun `default versions can be retrieved`() {
-    val soft = SoftAssertions()
-    soft.assertThat(sharedLibraryExtension.groovyVersion).isEqualTo(INITIAL_GROOVY_VERSION)
-    soft.assertThat(sharedLibraryExtension.coreVersion).isEqualTo(INITIAL_CORE_VERSION)
-    soft.assertThat(sharedLibraryExtension.globalLibPluginVersion).isEqualTo(
-      INITIAL_GLOBAL_LIB_VERSION)
-    soft.assertThat(sharedLibraryExtension.testHarnessVersion).isEqualTo(
-      INITIAL_TEST_HARNESS_VERSION)
-    soft.assertAll()
+    softlyAssert {
+      assertThat(sharedLibraryExtension.groovyVersion).isEqualTo(INITIAL_GROOVY_VERSION)
+      assertThat(sharedLibraryExtension.coreVersion).isEqualTo(INITIAL_CORE_VERSION)
+      assertThat(sharedLibraryExtension.globalLibPluginVersion).isEqualTo(
+        INITIAL_GLOBAL_LIB_VERSION)
+      assertThat(sharedLibraryExtension.testHarnessVersion).isEqualTo(
+        INITIAL_TEST_HARNESS_VERSION)
+      assertAll()
+    }
   }
 
-  @NotImplementedYet
   @Test
   internal fun `can set Groovy version`() {
+    sharedLibraryExtension.groovyVersion = "newVersion"
+
+    assertThat(sharedLibraryExtension.groovyVersion).isEqualTo("newVersion")
   }
 
-  @NotImplementedYet
   @Test
   internal fun `can set Jenkins core version`() {
+    sharedLibraryExtension.coreVersion = "newVersion"
+
+    assertThat(sharedLibraryExtension.coreVersion).isEqualTo("newVersion")
   }
 
-  @NotImplementedYet
   @Test
   internal fun `can set Global Library Plugin version`() {
+    sharedLibraryExtension.globalLibPluginVersion= "newVersion"
+
+    assertThat(sharedLibraryExtension.globalLibPluginVersion).isEqualTo("newVersion")
   }
 
-  @NotImplementedYet
   @Test
   internal fun `can set Jenkins Test Harness version`() {
+    sharedLibraryExtension.testHarnessVersion= "newVersion"
+
+    assertThat(sharedLibraryExtension.testHarnessVersion).isEqualTo("newVersion")
+  }
+
+  private fun softlyAssert(assertions: SoftAssertions.() -> Unit) {
+    val softAssertions = SoftAssertions()
+    softAssertions.assertions()
+    softAssertions.assertAll()
   }
 }
