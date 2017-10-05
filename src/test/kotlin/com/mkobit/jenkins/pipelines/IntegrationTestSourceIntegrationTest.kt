cb6c34623243fe2d56ad2c6d94828dd3b8ec1fa6
Mike Kobit <mkobit@gmail.com>
2017-10-04 20:21:42 -0500

    Add link to related issue for disabled test

--- src/test/kotlin/com/mkobit/jenkins/pipelines/IntegrationTestSourceIntegrationTest.kt
+++ src/test/kotlin/com/mkobit/jenkins/pipelines/IntegrationTestSourceIntegrationTest.kt
@@ -141,7 +141,7 @@ internal class IntegrationTestSourceIntegrationTest {
     gradleRunner.buildWith(arguments = listOf("-i"))
   }
 
-  @Disabled("This example works in a local Gradle project, but not with Gradle Test Kit")
+  @Disabled("This example works in a local Gradle project, but not with Gradle Test Kit. See https://github.com/gradle/kotlin-dsl/issues/492")
   @Test
   internal fun `Kotlin DSL extension configuration`(@GradleProject gradleRunner: GradleRunner) {
     gradleRunner.buildWith(arguments = listOf("-i"))
