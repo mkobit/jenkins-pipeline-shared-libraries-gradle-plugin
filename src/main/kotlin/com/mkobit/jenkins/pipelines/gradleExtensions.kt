3a7f2dceab5113b2e157637829571de880c0ed86
Mike Kobit <mkobit@gmail.com>
2017-10-20 15:36:21 -0500

    Remove unused imports from `gradleExtensions` file

--- src/main/kotlin/com/mkobit/jenkins/pipelines/gradleExtensions.kt
+++ src/main/kotlin/com/mkobit/jenkins/pipelines/gradleExtensions.kt
@@ -1,13 +1,7 @@
 package com.mkobit.jenkins.pipelines
 
 import org.gradle.api.Project
-import org.gradle.api.file.SourceDirectorySet
-import org.gradle.api.internal.HasConvention
 import org.gradle.api.provider.Property
-import org.gradle.api.provider.PropertyState
-import org.gradle.api.tasks.GroovySourceSet
-import org.gradle.api.tasks.SourceSet
-
 
 inline internal fun <reified T> Project.initializedProperty(initialState: T): Property<T> = this.objects.property(T::class.java).apply {
   set(initialState)
