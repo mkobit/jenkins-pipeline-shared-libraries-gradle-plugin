73d3b19c644853bb104e10d5889c53b5d40fdb75
Mike Kobit <mkobit@gmail.com>
2018-03-02 15:14:24 -0600

    Break out plugin into smaller chunks, close reponse objects, and add some KDoc

--- src/main/kotlin/com/mkobit/jenkins/pipelines/JenkinsIntegrationPlugin.kt
+++ src/main/kotlin/com/mkobit/jenkins/pipelines/JenkinsIntegrationPlugin.kt
@@ -11,6 +11,7 @@ import okhttp3.Response
 import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.Task
 import org.gradle.kotlin.dsl.invoke
 import org.gradle.kotlin.dsl.property
 import java.net.URL
@@ -22,7 +23,7 @@ internal open class JenkinsIntegrationPlugin : Plugin<Project> {
   }
   override fun apply(target: Project) {
     target.run {
-      val integrationExtension = extensions.create(
+      val integration = extensions.create(
         EXTENSION_NAME,
         JenkinsIntegrationExtension::class.java,
         objects.property<URL>(),
@@ -32,52 +33,74 @@ internal open class JenkinsIntegrationPlugin : Plugin<Project> {
 
       tasks {
         "retrieveJenkinsGdsl" {
-          description = "Downloads the Jenkins Pipeline GDSL from the Jenkins instance"
-          inputs.property("url", integrationExtension.baseUrl)
-          outputs.file(integrationExtension.downloadDirectory.file("idea.gdsl"))
-          outputs.upToDateWhen { false }
-          doLast {
-            val response = downloadGdsl(HttpUrl.get(
-              integrationExtension.baseUrl.get())!!,
-              integrationExtension.authentication.getOrElse(AnonymousAuthentication)
-            )
-            if (!response.isSuccessful) throw GradleException("Error retrieving GDSL. ${response.statusLineAsMessage}")
-            integrationExtension.downloadDirectory.file("idea.gdsl").get().asFile.writeBytes(response.body()!!.bytes())
-          }
+          setupRetrieveJenkinsGdsl(integration, this)
         }
 
         "retrieveJenkinsPluginData" {
-          description = "Downloads the Jenkins plugin data from the Jenkins instance"
-          inputs.property("url", integrationExtension.baseUrl)
-          outputs.file(integrationExtension.downloadDirectory.file("plugins.json"))
-          outputs.upToDateWhen { false }
-          doLast {
-            val response = retrievePluginManagerData(HttpUrl.get(
-              integrationExtension.baseUrl.get())!!,
-              integrationExtension.authentication.getOrElse(AnonymousAuthentication)
-            )
-            if (!response.isSuccessful) {
-              val userDetails = response.headers().xYouAreAuthenticatedAs?.let { "User: $it" }
-              val requiredPermission = response.headers().xRequiredPermission?.let { "Permission required: $it" }
-              val permissionImpliedBy = response.headers().xPermissionImpliedBy.run {
-                if (isNotEmpty()) {
-                  joinToString(System.lineSeparator()) { "Permission implied by: $it" }
-                } else {
-                  null
-                }
+          setupRetrieveJenkinsPluginData(integration, this)
+        }
+      }
+    }
+  }
+
+  private fun setupRetrieveJenkinsGdsl(integration: JenkinsIntegrationExtension, task: Task) {
+    task.apply {
+      description = "Downloads the Jenkins Pipeline GDSL from the Jenkins instance"
+      inputs.property("url", integration.baseUrl)
+      outputs.file(integration.downloadDirectory.file("idea.gdsl"))
+      outputs.upToDateWhen { false }
+      doLast {
+        downloadGdsl(HttpUrl.get(
+          integration.baseUrl.get())!!,
+          integration.authentication.getOrElse(AnonymousAuthentication)
+        ).use { response ->
+          if (!response.isSuccessful) throw GradleException("Error retrieving GDSL. ${response.statusLineAsMessage}")
+          integration.downloadDirectory
+            .file("idea.gdsl")
+            .get()
+            .asFile.
+            writeBytes(response.body()!!.bytes())
+        }
+      }
+    }
+  }
+  
+  private fun setupRetrieveJenkinsPluginData(integration: JenkinsIntegrationExtension, task: Task) {
+    task.apply {
+      description = "Downloads the Jenkins plugin data from the Jenkins instance"
+      inputs.property("url", integration.baseUrl)
+      outputs.file(integration.downloadDirectory.file("plugins.json"))
+      outputs.upToDateWhen { false }
+      doLast {
+        retrievePluginManagerData(HttpUrl.get(
+          integration.baseUrl.get())!!,
+          integration.authentication.getOrElse(AnonymousAuthentication)
+        ).use { response ->
+          if (!response.isSuccessful) {
+            val userDetails = response.headers().xYouAreAuthenticatedAs?.let { "User: $it" }
+            val requiredPermission = response.headers().xRequiredPermission?.let { "Permission required: $it" }
+            val permissionImpliedBy = response.headers().xPermissionImpliedBy.run {
+              if (isNotEmpty()) {
+                joinToString(System.lineSeparator()) { "Permission implied by: $it" }
+              } else {
+                null
               }
-              val errorSubject = when (response.code()) {
-                401 -> "Unable to authenticate due to invalid password/token ${response.statusLineAsMessage}"
-                403 -> "Unauthorized to retrieve plugin data ${response.statusLineAsMessage}"
-                else ->"Error downloading plugin data ${response.statusLineAsMessage}"
-              }
-              val errorMessage = listOf(errorSubject, userDetails, requiredPermission, permissionImpliedBy)
-                .filter { it != null }
-                .joinToString(System.lineSeparator())
-              throw GradleException(errorMessage)
             }
-            integrationExtension.downloadDirectory.file("plugins.json").get().asFile.writeBytes(response.body()!!.bytes())
+            val errorSubject = when (response.code()) {
+              401 -> "Unable to authenticate due to invalid password/token ${response.statusLineAsMessage}"
+              403 -> "Unauthorized to retrieve plugin data ${response.statusLineAsMessage}"
+              else -> "Error downloading plugin data ${response.statusLineAsMessage}"
+            }
+            val errorMessage = listOf(errorSubject, userDetails, requiredPermission, permissionImpliedBy)
+              .filter { it != null }
+              .joinToString(System.lineSeparator())
+            throw GradleException(errorMessage)
           }
+          integration.downloadDirectory
+            .file("plugins.json")
+            .get()
+            .asFile
+            .writeBytes(response.body()!!.bytes())
         }
       }
     }
--- src/main/kotlin/com/mkobit/jenkins/pipelines/http/credentials.kt
+++ src/main/kotlin/com/mkobit/jenkins/pipelines/http/credentials.kt
@@ -3,15 +3,24 @@ package com.mkobit.jenkins.pipelines.http
 import okhttp3.Credentials
 
 /**
- * Provides authentication for an HTTP request.
+ * Authentication for an HTTP request.
+ * This only supports preemptive authentication
+ * @see BasicAuthentication
+ * @see ApiTokenAuthentication
+ * @see AnonymousAuthentication
  */
 interface Authentication {
+  /**
+   * Provides the headers for preemptive authentication.
+   */
   fun headers(): Map<String, String>
 }
 
 /**
  * Basic authentication authentication.
  * It is generally preferred to use an API key [ApiTokenAuthentication].
+ * @property username the username to authenticate with
+ * @property password the password for the [username]
  */
 data class BasicAuthentication(val username: String, val password: String) : Authentication {
   override fun headers(): Map<String, String> = mapOf("Authentication" to Credentials.basic(username, password))
@@ -19,6 +28,8 @@ data class BasicAuthentication(val username: String, val password: String) : Aut
 
 /**
  * Authentication using API token.
+ * @property username the username to authenticate with
+ * @property apiToken the API token for the [username]
  */
 data class ApiTokenAuthentication(val username: String, val apiToken: String) : Authentication {
   override fun headers(): Map<String, String> = mapOf("Authentication" to Credentials.basic(username, apiToken))
