7585b1b04f56595990dd7e191a888fd77d006d86
Mike Kobit <mkobit@gmail.com>
2018-04-10 12:46:14 -0500

    ⇡ build scan plugin from `1.13` to `1.13.1`

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ import java.io.ByteArrayOutputStream
 import java.net.URL
 
 plugins {
-  id("com.gradle.build-scan") version "1.13"
+  id("com.gradle.build-scan") version "1.13.1"
   `kotlin-dsl`
   `java-library`
   `java-gradle-plugin`
