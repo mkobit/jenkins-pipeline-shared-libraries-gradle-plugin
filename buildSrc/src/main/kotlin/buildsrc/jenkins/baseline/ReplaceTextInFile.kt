bd5d6a2e05a5f97979dc0a838fb2473686880c98
Mike Kobit <mkobit@gmail.com>
2018-11-30 14:32:46 -0600

    Fix `buildSrc` task for automated code replacement

--- buildSrc/src/main/kotlin/buildsrc/jenkins/baseline/ReplaceTextInFile.kt
+++ buildSrc/src/main/kotlin/buildsrc/jenkins/baseline/ReplaceTextInFile.kt
@@ -10,6 +10,7 @@ import org.gradle.api.tasks.InputFile
 import org.gradle.api.tasks.Internal
 import org.gradle.api.tasks.Nested
 import org.gradle.api.tasks.TaskAction
+import org.gradle.kotlin.dsl.listProperty
 import java.nio.file.Files
 import javax.inject.Inject
 
@@ -18,7 +19,7 @@ open class ReplaceTextInFile @Inject constructor(
 ) : DefaultTask() {
 
   @get:Nested
-  val replacements: ListProperty<Replacement> = objectFactory.listProperty(Replacement::class.java)
+  val replacements: ListProperty<Replacement> = objectFactory.listProperty<Replacement>().empty()
 
   @get:InputFile
   val targetFile: RegularFileProperty = objectFactory.fileProperty()
