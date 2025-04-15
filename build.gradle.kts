1cf1c7640c635b37068f5b42fd7db920cf8bdf35
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2025-04-14 18:48:24 -0700

    chore: use toolchain instead of compatibility settings for java

--- build.gradle.kts
+++ build.gradle.kts
@@ -65,8 +65,9 @@ buildScan {
 }
 
 java {
-  sourceCompatibility = JavaVersion.VERSION_1_8
-  targetCompatibility = JavaVersion.VERSION_1_8
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(8))
+  }
 }
 
 sourceSets {
