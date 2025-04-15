8968b27370cdadc0eeb7d87a4473ebdfd0784334
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2025-04-15 04:53:13 -0700

    chore: format UnitTestSourceIntegrationTest.kt

--- src/test/kotlin/com/mkobit/jenkins/pipelines/UnitTestSourceIntegrationTest.kt
+++ src/test/kotlin/com/mkobit/jenkins/pipelines/UnitTestSourceIntegrationTest.kt
@@ -1,7 +1,5 @@
 package com.mkobit.jenkins.pipelines
 
-import com.mkobit.gradle.test.kotlin.testkit.runner.build
-import com.mkobit.gradle.test.kotlin.testkit.runner.info
 import org.gradle.testkit.runner.BuildResult
 import org.gradle.testkit.runner.GradleRunner
 import org.junit.jupiter.api.Disabled
@@ -15,15 +13,21 @@ import strikt.gradle.testkit.output
 import strikt.gradle.testkit.task
 import testsupport.junit.ForGradleVersions
 import testsupport.junit.GradleProject
+import testsupport.testkit.runner.build
+import testsupport.testkit.runner.info
 
 @ForGradleVersions
 internal class UnitTestSourceIntegrationTest {
 
   @TestTemplate
-  internal fun `can write unit tests using JenkinsPipelineUnit`(@GradleProject(["projects", "jenkins-pipeline-unit"]) gradleRunner: GradleRunner) {
-    val buildResult: BuildResult = gradleRunner.apply {
-      info = true
-    }.build("test")
+  internal fun `can write unit tests using JenkinsPipelineUnit`(
+    @GradleProject(["projects", "jenkins-pipeline-unit"]) gradleRunner: GradleRunner
+  ) {
+    val buildResult: BuildResult =
+      gradleRunner
+        .apply {
+          info = true
+        }.build("test")
 
     expectThat(buildResult)
       .task(":test")
@@ -32,19 +36,27 @@ internal class UnitTestSourceIntegrationTest {
   }
 
   @TestTemplate
-  internal fun `integrationTest task is not executed when test is executed`(@GradleProject(["projects", "only-plugins-block"]) gradleRunner: GradleRunner) {
-    val buildResult: BuildResult = gradleRunner.apply {
-      info = true
-    }.build("test")
+  internal fun `integrationTest task is not executed when test is executed`(
+    @GradleProject(["projects", "only-plugins-block"]) gradleRunner: GradleRunner
+  ) {
+    val buildResult: BuildResult =
+      gradleRunner
+        .apply {
+          info = true
+        }.build("test")
 
     expectThat(buildResult)
       .task(":integrationTest")
       .isNull()
   }
 
-  @Disabled("@Grab does not seem to be working with tests in Gradle. See https://stackoverflow.com/questions/16471096/any-alternative-to-grabconfig and https://stackoverflow.com/questions/4611230/no-suitable-classloader-found-for-grab")
+  @Disabled(
+    "@Grab does not seem to be working with tests in Gradle. See https://stackoverflow.com/questions/16471096/any-alternative-to-grabconfig and https://stackoverflow.com/questions/4611230/no-suitable-classloader-found-for-grab",
+  )
   @TestTemplate
-  internal fun `can test @Grab using source can be unit tested normally`(@GradleProject(["projects", "source-with-@grab"]) gradleRunner: GradleRunner) {
+  internal fun `can test @Grab using source can be unit tested normally`(
+    @GradleProject(["projects", "source-with-@grab"]) gradleRunner: GradleRunner
+  ) {
     val buildResult = gradleRunner.build("test", "--tests", "*GrabUsingLibraryTest*")
 
     expectThat(buildResult)
@@ -54,7 +66,9 @@ internal class UnitTestSourceIntegrationTest {
   }
 
   @TestTemplate
-  internal fun `can run tests that do not use the source code used by @Grab`(@GradleProject(["projects", "source-with-@grab"]) gradleRunner: GradleRunner) {
+  internal fun `can run tests that do not use the source code used by @Grab`(
+    @GradleProject(["projects", "source-with-@grab"]) gradleRunner: GradleRunner
+  ) {
     val buildResult = gradleRunner.build("test", "--tests", "*NonGrabUsingTest*")
 
     expectThat(buildResult)
@@ -64,7 +78,9 @@ internal class UnitTestSourceIntegrationTest {
   }
 
   @TestTemplate
-  internal fun `can test library code that makes use of Jenkins core and plugin classes`(@GradleProject(["projects", "global-library-using-jenkins-plugin-classes"]) gradleRunner: GradleRunner) {
+  internal fun `can test library code that makes use of Jenkins core and plugin classes`(
+    @GradleProject(["projects", "global-library-using-jenkins-plugin-classes"]) gradleRunner: GradleRunner
+  ) {
     val buildResult = gradleRunner.build("test")
 
     expectThat(buildResult)
