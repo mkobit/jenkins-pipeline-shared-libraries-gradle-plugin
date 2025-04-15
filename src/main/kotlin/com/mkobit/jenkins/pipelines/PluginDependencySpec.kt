fd6f942a470ee8c538a33462dd963216943b2a50
Alberto Cavalcante <54247214+albertocavalcante@users.noreply.github.com>
2025-04-15 04:13:05 -0700

    chore: formar PluginDependencySpec.kt

--- src/main/kotlin/com/mkobit/jenkins/pipelines/PluginDependencySpec.kt
+++ src/main/kotlin/com/mkobit/jenkins/pipelines/PluginDependencySpec.kt
@@ -20,19 +20,21 @@ import javax.inject.Inject
  * @property workflowStepApiPluginVersion [Pipeline: Step API](https://plugins.jenkins.io/workflow-step-api) plugin version
  * @property workflowSupportPluginVersion [Pipeline: Supporting APIs](https://plugins.jenkins.io/workflow-support) plugin version
  */
-open class PluginDependencySpec @Inject constructor(
-  val workflowApiPluginVersion: Property<String>,
-  val workflowBasicStepsPluginVersion: Property<String>,
-  val workflowCpsPluginVersion: Property<String>,
-  val workflowDurableTaskStepPluginVersion: Property<String>,
-  val workflowCpsGlobalLibraryPluginVersion: Property<String>,
-  val workflowJobPluginVersion: Property<String>,
-  val workflowMultibranchPluginVersion: Property<String>,
-  val workflowScmStepPluginVersion: Property<String>,
-  val workflowStepApiPluginVersion: Property<String>,
-  val workflowSupportPluginVersion: Property<String>,
-  private val objectFactory: ObjectFactory
-) {
+open class PluginDependencySpec
+  @Inject
+  constructor(
+    val workflowApiPluginVersion: Property<String>,
+    val workflowBasicStepsPluginVersion: Property<String>,
+    val workflowCpsPluginVersion: Property<String>,
+    val workflowDurableTaskStepPluginVersion: Property<String>,
+    val workflowCpsGlobalLibraryPluginVersion: Property<String>,
+    val workflowJobPluginVersion: Property<String>,
+    val workflowMultibranchPluginVersion: Property<String>,
+    val workflowScmStepPluginVersion: Property<String>,
+    val workflowStepApiPluginVersion: Property<String>,
+    val workflowSupportPluginVersion: Property<String>,
+    private val objectFactory: ObjectFactory
+  ) {
   // .empty() needed due to https://github.com/gradle/gradle/issues/7485
   private val additionalDependencies: ListProperty<PluginDependency> = objectFactory.listProperty<PluginDependency>().empty()
 
@@ -42,28 +44,41 @@ open class PluginDependencySpec @Inject constructor(
    * @param name the name of the plugin artifact
    * @param version the version of the plugin to use
    */
-  fun dependency(group: String, name: String, version: String) {
+  fun dependency(
+      group: String,
+      name: String,
+      version: String
+  ) {
     additionalDependencies.add(PluginDependency(group, name, version))
   }
 
   private fun workflowApiPluginDependency(): Provider<String> =
     workflowApiPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-api:$it" }
+
   private fun workflowBasicStepsPluginDependency(): Provider<String> =
     workflowBasicStepsPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-basic-steps:$it" }
+
   private fun workflowCpsPluginDependency(): Provider<String> =
     workflowCpsPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-cps:$it" }
+
   private fun workflowDurableTaskStepPluginDependency(): Provider<String> =
     workflowDurableTaskStepPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-durable-task-step:$it" }
+
   private fun workflowGlobalCpsLibraryPluginDependency(): Provider<String> =
     workflowCpsGlobalLibraryPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-cps-global-lib:$it" }
+
   private fun workflowJobPluginDependency(): Provider<String> =
     workflowJobPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-job:$it" }
+
   private fun workflowMultibranchPluginDependency(): Provider<String> =
     workflowMultibranchPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-multibranch:$it" }
+
   private fun workflowScmStepPluginDependency(): Provider<String> =
     workflowScmStepPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-scm-step:$it" }
+
   private fun workflowStepApiPluginDependency(): Provider<String> =
     workflowStepApiPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-step-api:$it" }
+
   private fun workflowSupportPluginDependency(): Provider<String> =
     workflowSupportPluginVersion.map { "org.jenkins-ci.plugins.workflow:workflow-support:$it" }
 
