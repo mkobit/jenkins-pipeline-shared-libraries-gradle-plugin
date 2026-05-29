34817d06666f18f05b13160347b41f9e5ac2f33d
Mike Kobit <mkobit@gmail.com>
2026-05-28 22:37:56 -0500

    chore(main): release 0.12.0

--- gradle/version.gradle.kts
+++ gradle/version.gradle.kts
@@ -1,3 +1,3 @@
 // Declared here for release-please automatic version management — see
 // https://github.com/googleapis/release-please/blob/203919b5638144b1011a308bfce8b8b5c271a7c1/docs/customizing.md?plain=1#L174
-project.version = "0.11.0" // x-release-please-version
+project.version = "0.12.0" // x-release-please-version
